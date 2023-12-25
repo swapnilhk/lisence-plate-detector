388248c47fc36531f3585c83b93751a5a6927011
Swapnil Kharabe <swapnilhk@gmail.com>
2023-12-25 12:31:13 +0000

    modulerized code

--- src/Yolov6Util.py
+++ src/Yolov6Util.py
@@ -2,7 +2,16 @@ import torch, math
 import numpy as np
 from yolov6.data.data_augment import letterbox
 
-class Yolov6Util:
+from yolov6.core.inferer import Inferer
+from typing import List, Optional
+from yolov6.utils.nms import non_max_suppression
+
+conf_thres: float =.25 #@param {type:"number"}
+iou_thres: float =.45 #@param {type:"number"}
+max_det:int =  1000 #@param {type:"integer"}
+agnostic_nms: bool = False #@param {type:"boolean"}
+
+class ImageProcessor:
   def __init__():
     #Set-up hardware options
     cuda = device != 'cpu' and torch.cuda.is_available()
@@ -32,3 +41,34 @@ class Yolov6Util:
     image = image.half() if half else image.float()  # uint8 to fp16/32
     image /= 255  # 0 - 255 to 0.0 - 1.0
     return image, img_src
+
+class FrameProcessor:
+  def __init__(self, img_size, stride, half, device, model):
+    self.model = model
+    self.img_size = img_size
+    self.stride = stride
+    cuda = device != 'cpu' and torch.cuda.is_available()
+    self.device = torch.device('cuda:0' if cuda else 'cpu')
+    if half & (device != 'cpu'):
+      self.model.model.half()
+    else:
+      self.model.model.float()
+      self.half = False
+    self.model(torch.zeros(1, 3, *img_size).to(device).type_as(next(model.model.parameters())))  # warmup
+                
+  def process_frame(self, frame):
+    detections = []
+    img, img_src = ImageProcessor.process_image(frame, self.img_size, self.stride, self.half)
+    img = img.to(self.device)
+    if len(img.shape) == 3:
+      img = img[None]
+      # expand for batch dim
+    pred_results = self.model(img)
+    classes:Optional[List[int]] = None # the classes to keep
+    det = non_max_suppression(pred_results, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)[0]
+    gn = torch.tensor(img_src.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+    if len(det):
+      det[:, :4] = Inferer.rescale(img.shape[2:], det[:, :4], img_src.shape).round()
+      for x1, y1, x2, y2, conf, cls in reversed(det):
+        detections.append([x1, y1, x2, y2, conf, cls])
+    return detections
--- src/test_video.py
+++ src/test_video.py
@@ -1,23 +1,12 @@
 import os
 import cv2
-import torch
 from yolov6.layers.common import DetectBackend
-from yolov6.core.inferer import Inferer
-from Yolov6Util import Yolov6Util
+from Yolov6Util import FrameProcessor,  ImageProcessor
 from yolov6.utils.events import load_yaml
-from yolov6.core.inferer import Inferer
-from typing import List, Optional
-from yolov6.utils.nms import non_max_suppression
 
 device:str = "cpu" #@param ["gpu", "cpu"]
 half:bool = False #@param {type:"boolean"}
 img_size:int = 640 #@param {type:"integer"}
-conf_thres: float =.25 #@param {type:"number"}
-iou_thres: float =.45 #@param {type:"number"}
-max_det:int =  1000 #@param {type:"integer"}
-agnostic_nms: bool = False #@param {type:"boolean"}
-hide_labels: bool = False #@param {type:"boolean"}
-hide_conf: bool = False #@param {type:"boolean"}
 
 VIDEOS_DIR = os.path.join('/Users/sw20039189/Movies/', 'videos')
 video_path = os.path.join(VIDEOS_DIR, 'traffic.mp4')
@@ -31,35 +20,16 @@ model = DetectBackend(f"license_plate_detector_ckpt.pt", device=device)
 stride = model.stride
 class_names = load_yaml("dataset_config.yaml")['names']
 
-if half & (device != 'cpu'):
-  model.model.half()
-else:
-  model.model.float()
-  half = False
+img_size = ImageProcessor.check_img_size(img_size, s=stride)
 
-if device != 'cpu':
-  model(torch.zeros(1, 3, *img_size).to(device).type_as(next(model.model.parameters())))  # warmup
-
-img_size = Yolov6Util.check_img_size(img_size, s=stride)
+frameProcessor = FrameProcessor(img_size, stride, half,  device, model)
 
 while ret:
-  img, img_src = Yolov6Util.process_image(frame, img_size, stride, half)
-
-  img = img.to(device)
-
-  if len(img.shape) == 3:
-      img = img[None]
-      # expand for batch dim
-  pred_results = model(img)
-  classes:Optional[List[int]] = None # the classes to keep
-  det = non_max_suppression(pred_results, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)[0]
-
-  if len(det):
-    det[:, :4] = Inferer.rescale(img.shape[2:], det[:, :4], img_src.shape).round()
-    for x1, y1, x2, y2, conf, cls in reversed(det):
-        cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
-        class_num = int(cls)
-        cv2.putText(frame, class_names[class_num].upper(), (int(x1), int(y1 - 10)),
+  for x1, y1, x2, y2, conf, cls in frameProcessor.process_frame(frame):
+    if float(conf) > 0.6:
+      cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+      class_num = int(cls)
+      cv2.putText(frame, class_names[class_num].upper(), (int(x1), int(y1 - 10)),
                         cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
   out.write(frame)
   ret, frame = cap.read()
